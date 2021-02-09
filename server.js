4e0d15d51f07250fd059d6fe4aae6dccc29f23b7
chunga <clintonchunga@gmail.com>
2021-02-09 10:12:36 +0100

    adding my server.js file after installing my dependences

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+//! require("./seeders/seed"); // Either npm run seed or keep this in here uncommented and it will run the seed
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
+app.use(logger("dev"));
+// to be able to use json format
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//require(apiRoute)(app);
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
